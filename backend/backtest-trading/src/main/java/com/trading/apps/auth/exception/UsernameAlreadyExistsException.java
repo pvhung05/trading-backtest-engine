1c977ab9740dd8cca2c86d4e62cba3c43d2f636d
Pham Van Hung <phamvanhung242005@gmail.com>
2026-05-22 11:29:25 +0700

    feat(backend): auth exception #10

---
+++ backend/backtest-trading/src/main/java/com/trading/apps/auth/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.trading.apps.auth.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
